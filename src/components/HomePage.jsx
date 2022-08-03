f3ced7994008463253a6bd3cf8105a791e7e7062
frogosa-alea <benitezalea.f@gmail.com>
2022-08-04 00:48:01 +0800

    Cryptocurrencies Component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
-
+import { Cryptocurrencies, News } from '../components'
 import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const { Title } = Typography;
 
 const HomePage = () => {
 
-  const { data, isFetching } = useGetCryptosQuery()
+  const { data, isFetching } = useGetCryptosQuery(10)
 
   const globalStats = data?.data?.stats
 
@@ -34,6 +34,25 @@ const HomePage = () => {
           <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/>
         </Col>
       </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/cryptocurrencies'>Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified={true}/>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Latest Crypto News
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/news'>Show more</Link>
+        </Title>
+      </div>
+      <News simplified/>
+
     </>
   )
 }
